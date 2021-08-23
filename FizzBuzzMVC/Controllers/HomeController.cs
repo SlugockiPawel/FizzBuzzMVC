97c872e9c82c243e5756c9fad1aef7fa3f9bfd80
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-08-23 18:41:38 +0200

    implement fizz buzz logic

--- FizzBuzzMVC/Controllers/HomeController.cs
+++ FizzBuzzMVC/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace FizzBuzzMVC.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         [HttpGet]
@@ -50,7 +50,36 @@ namespace FizzBuzzMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult FbPage(FizzBuzz fizzBuzz)
         {
-            
+            List<string> fbItems = new();
+
+            bool fizz;
+            bool buzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                fizz = (i % fizzBuzz.FizzValue == 0);
+                buzz = (i % fizzBuzz.BuzzValue == 0);
+
+                if (fizz && buzz)
+                {
+                    fbItems.Add("FizzBuzz");
+                }
+                else if (fizz)
+                {
+                    fbItems.Add("Fizz");
+                }
+                else if (buzz)
+                {
+                    fbItems.Add("Buzz");
+                }
+                else
+                {
+                    fbItems.Add(i.ToString());
+                }
+
+                fizzBuzz.Result = fbItems;
+            }
+
             return View(fizzBuzz);
         }
     }
